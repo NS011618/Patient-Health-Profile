1938a5d901259bb26b3aeaf488207638197452ec
NS011618 <96785237+NS011618@users.noreply.github.com>
2023-09-15 00:50:01 +0530

    updating input data

--- src/pages/Inputdata.jsx
+++ src/pages/Inputdata.jsx
@@ -1,101 +1,137 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Papa from 'papaparse';
-
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-} from 'recharts';
+import { inputRoute } from '../utils/APIRoutes';
 import { styled } from 'styled-components';
 
-const Inputdata = () => {
-  const [data, setData] = useState([]);
+const CsvDataDisplay = () => {
+  const navigate = useNavigate();
+  const [csvData, setCsvData] = useState([]);
+  const [postDataResponse, setPostDataResponse] = useState(null);
   const [fileNames, setFileNames] = useState([]);
+  const [editedFileNames, setEditedFileNames] = useState([]);
 
-  const handleFileUpload = (e) => {
-    const files = e.target.files;
+  const handleFileUpload = (event) => {
+    const files = event.target.files;
+    const newFiles = Array.from(files);
+
+    // Create an array to store the names of uploaded files
+    const newFileNames = newFiles.map((file) => file.name);
+
+    // Initialize editedFileNames with the same values as newFileNames
+    setEditedFileNames([...newFileNames]);
+
+    // Append the new file names to the existing file names
+    setFileNames((prevFileNames) => [...prevFileNames, ...newFileNames]);
 
     // Loop through selected files and parse each one
-    for (let i = 0; i < files.length; i++) {
-      const file = files[i];
-
-      // Update the fileNames array with the name of the current file
-      setFileNames((prevFileNames) => [...prevFileNames, file.name]);
-
+    newFiles.forEach((file) => {
       Papa.parse(file, {
         header: true,
         complete: (results) => {
           // Append the data from the current file to the existing data
-          setData((prevData) => [...prevData, ...results.data]);
+          setCsvData((prevData) => [...prevData, ...results.data]);
         },
       });
+    });
+  };
+
+  const handleEditFileName = (index) => {
+    // Update the fileNames state with the edited name at the specified index
+    setFileNames((prevFileNames) => {
+      const newFileNames = [...prevFileNames];
+      newFileNames[index] = editedFileNames[index];
+      return newFileNames;
+    });
+  };
+
+  const postDataToPublicURL = async () => {
+    if (csvData.length === 0) {
+      setPostDataResponse('No data to post');
+      return;
+    }
+
+    try {
+      const response = await fetch(inputRoute, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(csvData),
+      });
+
+      if (response.ok) {
+        setPostDataResponse('Data posted successfully');
+        navigate('/patient-dashboard');
+      } else {
+        setPostDataResponse('Failed to post data');
+      }
+    } catch (error) {
+      console.error('Error posting data:', error);
+      setPostDataResponse('An error occurred while posting data');
     }
   };
 
   return (
-    <>
+    <div>
+      <h1>CSV Data Display and Post</h1>
       <input type="file" accept=".csv" multiple onChange={handleFileUpload} />
       {fileNames.length > 0 && (
         <div>
           <h2>Selected Files:</h2>
           <ul>
             {fileNames.map((fileName, index) => (
-              <li key={index}>{fileName}</li>
+              <li key={index}>
+                <input
+                  type="text"
+                  value={editedFileNames[index]}
+                  onChange={(e) => {
+                    const newEditedFileNames = [...editedFileNames];
+                    newEditedFileNames[index] = e.target.value;
+                    setEditedFileNames(newEditedFileNames);
+                  }}
+                />
+                <button onClick={() => handleEditFileName(index)}>Save</button>
+                {editedFileNames[index] !== fileName && (
+                  <span> (Edited)</span>
+                )}
+              </li>
             ))}
           </ul>
         </div>
       )}
-      <FormContainer>
-        <br />
-        {data.length ? (
-          <table>
-            {/* ...Your table code */}
-          </table>
-        ) : null}
-        <div className="flex flex-col md:flex-row items-center justify-center mt-8 md:space-x-6">
-          <div className="bg-white p-6 rounded-lg shadow-md w-full md:w-1/2 mt-4 md:mt-0">
-            {/* ...Your first bar chart */}
-          </div>
-          <div className="bg-white p-6 rounded-lg shadow-md w-full md:w-1/2 mt-4 md:mt-0">
-            {/* ...Your second bar chart */}
-          </div>
-        </div>
-      </FormContainer>
-    </>
+
+      
+      <button onClick={postDataToPublicURL}>Post Data</button>
+      {postDataResponse && <p>{postDataResponse}</p>}
+    </div>
   );
 };
 
-
 const FormContainer = styled.div`
-    .container {
-        width: 100%;
-        height: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    table {
-        border-collapse: collapse;
-        border-spacing: 0;
-        width: 100%;
-        border: 1px solid #ddd;
-    }
-    th,
-    td {
-        text-align: left;
-        padding: 16px;
-        border: 1px solid #ddd;
-    }
-    thead,tr:nth-child(even) {
-        background-color: #f2f2f2;
-    } 
-    
-  
-
+  .container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+    width: 100%;
+    border: 1px solid #ddd;
+  }
+  th,
+  td {
+    text-align: left;
+    padding: 16px;
+    border: 1px solid #ddd;
+  }
+  thead,
+  tr:nth-child(even) {
+    background-color: #f2f2f2;
+  }
 `;
 
-export default Inputdata
+export default CsvDataDisplay;
